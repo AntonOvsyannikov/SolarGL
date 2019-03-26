fa3f0efeab417a0459f07b47048164bc7b181071
Ovsiannikov Anton <aovsiannikov@CORP.OT.RU>
2019-03-26 15:22:23 +0300

    commit

--- EngineGL/Renderer/RendererLinux.py
+++ EngineGL/Renderer/RendererLinux.py
@@ -4,12 +4,19 @@ from LibGL.Buffers import *
 def RenderSceneToImage(scene, size=(640, 480)):
     """ :type scene: SceneBase """
 
-    ctx = OSMesaCreateContext(OSMESA_RGBA, None)
+    width, height = size
 
+    ctx = OSMesaCreateContext(OSMESA_RGBA, None)
+    #ctx = OSMesaCreateContextExt(OSMESA_RGBA, 32, 0, 0, None)
+
+    buf = arrays.GLubyteArray.zeros((height, width, 4))
+    assert(OSMesaMakeCurrent(ctx, buf, GL_UNSIGNED_BYTE, width, height))
+    assert(OSMesaGetCurrentContext())
+    
     scene.InitRender(*size)
     scene.Render()
-
-    image = MakeImage(*ReadDefaultBuffer(*size))
+    
+    image = MakeImage(*ReadDefaultBuffer(width, height, False))
 
     OSMesaDestroyContext(ctx)
 
@@ -20,6 +27,8 @@ def _check_env():
     import os
     assert os.environ.get('PYOPENGL_PLATFORM') == 'osmesa'
 
+
 _check_env()
 
 if False: from EngineGL.Common import SceneBase
+
